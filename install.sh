03f41bc0aed70cc0fa826740c66901932428b435
Goku <rafaplayrj2@gmail.com>
2020-08-18 07:24:18 -0300

    finalizando o install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo cp /src/bash.command-not-found /etc
+sudo cp src/bash.command-not-found /etc
 
+sudo echo "if [ -f /etc/bash.command-not-found ]; then" >> /etc/bash.bashrc
 
+sudo echo "    . /etc/bash.command-not-found" >> /etc/bash.bashrc
+
+sudo echo "fi" >> /etc/bash.bashrc
+
+sudo source /etc/bash.bashrc
